7f4d3fef4157d9650c31528a5c996b8d96c2b8e9
GabrielHNE <gabrielhnespindola@gmail.com>
2022-04-14 22:37:56 -0300

    Alterado nome no swagger

--- backend/NewMeal/Startup.cs
+++ backend/NewMeal/Startup.cs
@@ -42,7 +42,7 @@ namespace NewMeal
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Shop", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NewMeal", Version = "v1" });
             });
 
             services.AddAuthentication(x =>
